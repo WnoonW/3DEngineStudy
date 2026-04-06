66db29dade5da6fe54ef5acf3262049c4bb33169
WnoonW <wonwoo0406@naver.com>
2026-04-06 17:22:32 +0900

    Update ECS.h

--- ECS.h
+++ ECS.h
@@ -1,51 +1,87 @@
 #pragma once
 #include <unordered_map>
-#include <vector>
 #include <typeindex>
-#include <memory>
+#include <any>
 #include <DirectXMath.h>
+#include <d3d12.h>
+#include <wrl.h>
+#include "d3dx12.h"
 
-// 1. 엔티티 정의 (단순한 ID)
 using Entity = uint32_t;
 
-// 2. 컴포넌트 정의 (데이터 구조체)
+// ----------------------------------------------------
+// [ 컴포넌트(Component) 정의 ]
+// ----------------------------------------------------
+
 struct TransformComponent {
     DirectX::XMFLOAT3 position = { 0.0f, 0.0f, 0.0f };
+    DirectX::XMFLOAT3 rotation = { 0.0f, 0.0f, 0.0f };
     DirectX::XMFLOAT3 scale = { 1.0f, 1.0f, 1.0f };
     DirectX::XMFLOAT3 velocity = { 0.0f, 0.0f, 0.0f };
+    DirectX::XMFLOAT4X4 worldMatrix = {};
 };
 
-struct GravityComponent {
-    float strength = -9.8f;
-    bool isActive = false;
+// 렌더링에 필요한 버퍼, 뷰, 루트 시그니처 등을 담는 데이터
+struct RenderComponent {
+    bool isUI = false;
+    bool isSelected = false;
+    UINT indexCount = 0;
+
+    D3D12_VERTEX_BUFFER_VIEW vertexBufferView;
+    D3D12_INDEX_BUFFER_VIEW indexBufferView;
+    Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature;
+    Microsoft::WRL::ComPtr<ID3D12PipelineState> pso;
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> descHeap;
+    UINT descriptorSize = 0;
+
+    // 상수 버퍼 매핑 주소
+    UINT8* pMappedConstantData = nullptr;
 };
 
-// 3. 레지스트리 (컴포넌트 저장소)
+// 충돌 및 피킹(Picking)을 위한 바운딩 박스
+struct AABBComponent {
+    DirectX::XMFLOAT3 min;
+    DirectX::XMFLOAT3 max;
+};
+
+// ----------------------------------------------------
+// [ 레지스트리(Registry) - 기존 구현 유지/보완 ]
+// ----------------------------------------------------
+
 class Registry {
+private:
+    uint32_t nextEntityId = 0;
+    // 타입에 따라 Entity -> Component 매핑을 저장
+    std::unordered_map<std::type_index, std::any> components;
+
 public:
     Entity CreateEntity() {
-        return nextEntity++;
+        return nextEntityId++;
     }
 
-    // 컴포넌트 추가/수정
     template<typename T>
     void AddComponent(Entity entity, T component) {
-        GetComponentMap<T>()[entity] = component;
+        auto type = std::type_index(typeid(T));
+        if (components.find(type) == components.end()) {
+            components[type] = std::unordered_map<Entity, T>();
+        }
+        auto& componentMap = std::any_cast<std::unordered_map<Entity, T>&>(components[type]);
+        componentMap[entity] = component;
     }
 
-    // 컴포넌트 가져오기
     template<typename T>
     T& GetComponent(Entity entity) {
-        return GetComponentMap<T>()[entity];
+        auto type = std::type_index(typeid(T));
+        auto& componentMap = std::any_cast<std::unordered_map<Entity, T>&>(components[type]);
+        return componentMap.at(entity);
     }
 
-    // 특정 컴포넌트를 가진 모든 엔티티 맵 반환
     template<typename T>
-    static std::unordered_map<Entity, T>& GetComponentMap() {
-        static std::unordered_map<Entity, T> components;
-        return components;
+    std::unordered_map<Entity, T>& GetComponentMap() {
+        auto type = std::type_index(typeid(T));
+        if (components.find(type) == components.end()) {
+            components[type] = std::unordered_map<Entity, T>();
+        }
+        return std::any_cast<std::unordered_map<Entity, T>&>(components[type]);
     }
-
-private:
-    Entity nextEntity = 0;
 };
